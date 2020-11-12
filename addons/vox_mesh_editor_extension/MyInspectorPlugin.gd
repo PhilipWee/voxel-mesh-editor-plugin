37d79cd156f09e6109cfe679659e871e78f1f69e
Philip Wee <philip.andrewweedewang@gmail.com>
2020-11-12 17:51:37 +0800

    Generate Array Semi Working
    TODO:
    Use values from object to initialise

--- addons/vox_mesh_editor_extension/MyInspectorPlugin.gd
+++ addons/vox_mesh_editor_extension/MyInspectorPlugin.gd
@@ -6,12 +6,12 @@ var RandomIntEditor = preload("res://addons/vox_mesh_editor_extension/RandomIntE
 
 func can_handle(object):
 	# We support all objects in this example.
-	return true
+	return object is VoxelMesh
 
 
 func parse_property(object, type, path, hint, hint_text, usage):
 	# We handle properties of type integer.
-	if type == TYPE_INT:
+	if type == TYPE_ARRAY:
 		# Create an instance of the custom property editor and register
 		# it to a specific property path.
 		add_property_editor(path, RandomIntEditor.new())
--- addons/vox_mesh_editor_extension/RandomIntEditor.gd
+++ addons/vox_mesh_editor_extension/RandomIntEditor.gd
@@ -4,9 +4,11 @@ extends EditorProperty
 # The main control for editing the property.
 var property_control = Button.new()
 # An internal value of the property.
-var current_value = 0
+var current_value = []
 # A guard against internal changes when the property is updated.
 var updating = false
+
+var cube_width = 1
 
 
 func _init():
@@ -15,8 +17,54 @@ func _init():
 	# Make sure the control is able to retain the focus.
 	add_focusable(property_control)
 	# Setup the initial state and connect to the signal to track changes.
-	property_control.text = "Value: " + str(current_value)
+	property_control.text = "Generate Array"
 	property_control.connect("pressed", self, "_on_button_pressed")
+	
+func raycast_get_hits(initial_pos,dir,space_state):
+	var result = {'position':initial_pos}
+	var cast_forward = true
+	var hits = []
+	#Do the casting for one
+	while !(result.empty() and !cast_forward):
+		if cast_forward:
+			result = space_state.intersect_ray(result['position'],result['position'] + dir*1e6)
+			if result.empty():
+				#No more hits in forward direction
+				result = {'position':initial_pos + dir*1e6}
+				cast_forward = false
+				continue
+			else:
+				hits.append(result)
+				continue
+		else:
+			result = space_state.intersect_ray(result['position'],initial_pos)
+			if result.empty():
+				#No more hits in backward direction
+				break
+			else:
+				hits.append(result)
+				continue
+	return hits
+
+func get_point_arr():
+	var spatial = Spatial.new()
+	get_tree().get_root().add_child(spatial)
+	var space_state =  spatial.get_world().direct_space_state
+	var state_arr = []
+	state_arr.resize(pow(cube_width,3))
+	
+	for i in range(cube_width):
+		for j in range(cube_width):
+			for k in range(cube_width):
+				var hits = raycast_get_hits(Vector3(i,j,k),Vector3.RIGHT,space_state)
+				if hits.size() % 2 == 0:
+					#Inside mesh
+					state_arr[i*cube_width*cube_width + j*cube_width + k] = 0
+				else:
+					#Outside mesh
+					state_arr[i*cube_width*cube_width + j*cube_width + k] = 1
+	spatial.free()
+	return state_arr
 
 
 func _on_button_pressed():
@@ -25,8 +73,9 @@ func _on_button_pressed():
 		return
 
 	# Generate a new random integer between 0 and 99.
-	current_value = randi() % 100
-	property_control.text = "Value: " + str(current_value)
+	current_value = get_point_arr()
+	property_control.text = "Generate Array"
+	print("Point Array Retrieved:" + str(current_value).substr(0,200))
 	emit_changed(get_edited_property(), current_value)
 
 
@@ -39,5 +88,5 @@ func update_property():
 	# Update the control with the new value.
 	updating = true
 	current_value = new_value
-	property_control.text = "Value: " + str(current_value)
+	property_control.text = "Generate Array"
 	updating = false
