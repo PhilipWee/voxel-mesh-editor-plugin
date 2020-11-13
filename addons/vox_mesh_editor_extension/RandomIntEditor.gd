444be3c547690702875b0af06877591ea117d3f3
Philip Wee <philip.andrewweedewang@gmail.com>
2020-11-13 17:53:06 +0800

    now works (Kinda)
    TODO:
    Need to make function start from arbitrary origin

--- addons/vox_mesh_editor_extension/RandomIntEditor.gd
+++ addons/vox_mesh_editor_extension/RandomIntEditor.gd
@@ -8,7 +8,10 @@ var current_value = []
 # A guard against internal changes when the property is updated.
 var updating = false
 
+var chunk_size = 0
 var cube_width = 1
+var origin = Vector3(0,0,0)
+var min_dist_between_ray_hits
 
 
 func _init():
@@ -24,17 +27,19 @@ func raycast_get_hits(initial_pos,dir,space_state):
 	var result = {'position':initial_pos}
 	var cast_forward = true
 	var hits = []
+	dir = dir.normalized()
 	#Do the casting for one
 	while !(result.empty() and !cast_forward):
 		if cast_forward:
-			result = space_state.intersect_ray(result['position'],result['position'] + dir*1e6)
+			result = space_state.intersect_ray(result['position'],result['position'] + dir*1e5)
 			if result.empty():
 				#No more hits in forward direction
-				result = {'position':initial_pos + dir*1e6}
+				result = {'position':initial_pos + dir*1e5}
 				cast_forward = false
 				continue
 			else:
 				hits.append(result)
+				result['position'] += dir*min_dist_between_ray_hits
 				continue
 		else:
 			result = space_state.intersect_ray(result['position'],initial_pos)
@@ -43,6 +48,7 @@ func raycast_get_hits(initial_pos,dir,space_state):
 				break
 			else:
 				hits.append(result)
+				result['position'] -= dir*min_dist_between_ray_hits
 				continue
 	return hits
 
@@ -51,18 +57,25 @@ func get_point_arr():
 	get_tree().get_root().add_child(spatial)
 	var space_state =  spatial.get_world().direct_space_state
 	var state_arr = []
-	state_arr.resize(pow(cube_width,3))
-	
-	for i in range(cube_width):
-		for j in range(cube_width):
-			for k in range(cube_width):
+	state_arr.resize(pow(chunk_size,3))
+	var i
+	var j
+	var k
+	for i_iterator in range(chunk_size):
+		i = origin.x + i_iterator*cube_width
+		for j_iterator in range(chunk_size):
+			j = origin.y + j_iterator*cube_width
+			for k_iterator in range(chunk_size):
+				k = origin.z + k_iterator*cube_width
+				#Actual code
 				var hits = raycast_get_hits(Vector3(i,j,k),Vector3.RIGHT,space_state)
+				var cur_arr_index = i_iterator*chunk_size*chunk_size + j_iterator*chunk_size + k_iterator
 				if hits.size() % 2 == 0:
-					#Inside mesh
-					state_arr[i*cube_width*cube_width + j*cube_width + k] = 0
-				else:
 					#Outside mesh
-					state_arr[i*cube_width*cube_width + j*cube_width + k] = 1
+					state_arr[cur_arr_index] = 0
+				else:
+					#Inside mesh
+					state_arr[cur_arr_index] = 90
 	spatial.free()
 	return state_arr
 
@@ -75,6 +88,11 @@ func _on_button_pressed():
 	# Generate a new random integer between 0 and 99.
 	current_value = get_point_arr()
 	property_control.text = "Generate Array"
+	#The plus one is so that the chunk size is by number of cubes, not number of points
+	chunk_size = get_edited_object().chunk_size + 1 
+	cube_width = get_edited_object().cube_width
+	min_dist_between_ray_hits = cube_width/4
+	origin = Vector3(0,0,0) #TODO make dependent on mesh starting location
 	print("Point Array Retrieved:" + str(current_value).substr(0,200))
 	emit_changed(get_edited_property(), current_value)
 
